671d2d3f33ae982a6e924330000047b3993cef2f
avijitpandey5 <128674742+avijitpandey5@users.noreply.github.com>
2024-02-13 18:45:58 +0530

    Add files via upload

---
+++ Find First Palindromic String in the Array.cpp
@@ -0,0 +1,33 @@
+class Solution {
+    bool solve(string str)
+    {
+        int s=0;
+        int e=str.length()-1;
+
+        while(s<e)
+        {
+            if(str[s]!=str[e])
+            return false;
+
+            s++;
+            e--;
+        }
+
+        return true;
+    }
+public:
+    string firstPalindrome(vector<string>& words) {
+
+        string st="";
+
+        for(int i=0;i<words.size();i++)
+        {
+            bool check=solve(words[i]);
+            if(check)
+            return words[i];
+        }
+
+        return st;
+        
+    }
+};
