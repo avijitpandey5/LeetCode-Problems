3794553bce707bcab15a8e2d93a13c4dcf451303
Avijit Pandey <128674742+avijitpandey5@users.noreply.github.com>
2024-04-07 19:25:03 +0530

    Add files via upload

---
+++ Valid Parenthesis String.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+
+        stack<char>st1;
+        stack<char>st2;
+
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]=='(')
+            st1.push(i);
+
+            else if(s[i]=='*')
+            st2.push(i);
+
+            else if(s[i]==')')
+            {
+                if(st1.empty() && st2.empty())
+                return false;
+
+                if(!st1.empty())
+                st1.pop();
+
+                else
+                st2.pop();
+            }
+        }
+
+        while(!st1.empty())
+        {
+            if(st2.empty())
+            return false;
+
+            if(st1.top()>st2.top())
+            return false;
+            
+            st1.pop();
+            st2.pop();
+
+        }
+
+        return true;
+    }
+};
